75af27d16bf846acdf0daa2bc49ef52affd9ae58
Narmatov Dinnur <runniddefault@gmail.com>
2024-12-29 02:32:19 +0600

    implement request for rates and weather

--- components/shared/weather-exchange.jsx
+++ components/shared/weather-exchange.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
 import Image from "next/image";
 import {cn} from "@/lib/utils";
+import { getWeather, exchangeRates } from '@/lib/fetchData';
 
-const WeatherExchange = ({className}) => {
+const WeatherExchange = async ({className}) => {
+    const weather = await getWeather().catch((error) => console.error(error));
+    const rates = await exchangeRates();
+
     return (
         <div className={cn(className)}>
             <div className="flex items-center gap-2 mr-6 lx:mb-0 mb-2">
-                <Image src="/sun.svg" alt="sun icon" width={22} height={22}/> <p className="text-[#101828] font-medium">+11 °С в Бишкеке</p>
+                <Image src="/sun.svg" alt="sun icon" width={22} height={22}/> 
+                <p className="text-[#101828] font-medium">
+                    {weather?.current_weather?.temperature} {weather?.current_weather_units?.temperature} в Бишкеке
+                </p>
             </div>
             <div className="flex items-center gap-6">
-                <div className="flex items-center gap-[5.5px] ">
-                    <div className="rounded-full bg-[#E0EBFF] p-[6.8]">
-                        <Image src="/ic_ruble.svg" alt="sun icon" width={15} height={15}/>
-                    </div>
-                    <p className="text-[#101828] font-medium">0.84</p>
-                </div>
-                <div className="flex items-center gap-[5.5px]">
-                    <div className="rounded-full bg-[#E0EBFF] p-[6.8]">
-                        <Image src="/ic_dollar.svg" alt="sun icon" width={15} height={15}/>
-                    </div>
-                    <p className="text-[#101828] font-medium">86.50</p>
-                </div>
-                <div className="flex items-center gap-[5.5px] ">
-                    <div className="rounded-full bg-[#E0EBFF] p-[6.8]">
-                        <Image src="/ic_euro.svg" alt="sun icon" width={15} height={15}/>
-                    </div>
-                    <p className="text-[#101828] font-medium">90.53</p>
-                </div>
+                {
+                    rates.map((item) => (
+                        <div className="flex items-center gap-[5.5px] ">
+                            <div className="rounded-full bg-[#E0EBFF] p-[6.8]">
+                                <Image src={item?.target_currency?.icon} alt="currency icon" width={15} height={15}/>
+                            </div>
+                            <p className="text-[#101828] font-medium">{item?.price_per}</p>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
