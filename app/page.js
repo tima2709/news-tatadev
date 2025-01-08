7b1664da36580593dd1b60a01dbc46be29ea32a3
Narmatov Dinnur <runniddefault@gmail.com>
2025-01-09 00:05:06 +0600

    fix bug when meta tags not found fields

--- app/page.js
+++ app/page.js
@@ -10,22 +10,22 @@ import {getMetaTags} from "@/lib/fetchData";
 export async function generateMetadata() {
     const data = await getMetaTags('home');
     return {
-        title: data.title || "Чуйские известия - Главные новости и события",
-        description:data.description || "Ежедневные новости, политики, экономики, общества, спорта и культуры. Актуальная информация и аналитика.",
-        keywords: data.keywords || "новости, Чуйские известия, политика, экономика, общество, происшествия",
+        title: data?.title || "Чуйские известия - Главные новости и события",
+        description:data?.description || "Ежедневные новости, политики, экономики, общества, спорта и культуры. Актуальная информация и аналитика.",
+        keywords: data?.keywords || "новости, Чуйские известия, политика, экономика, общество, происшествия",
         openGraph: {
-            title: data.title || "Project Meta Title",
-            description: data.description || "Project Meta Description",
+            title: data?.title || "Project Meta Title",
+            description: data?.description || "Project Meta Description",
             url: data?.url_path || "https://news.tatadev.dev/",
             type: "website",
-            images: [{ url: data.image || "/logo-image.svg" }],
+            images: [{ url: data?.image || "/logo-image.svg" }],
         },
         verification: {
             google: "string",
             yandex: "string",
         },
         icons: {
-            icon: data.image || "/logo-image.svg",
+            icon: data?.image || "/logo-image.svg",
         },
         authors: {
             name: "TataDev Team",
