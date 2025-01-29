182f86361e4d3a97bdd2a0b096807e0b302d6af7
tmrln2709 <tmrln2709@gmail.com>
2025-01-29 11:07:11 +0600

    fix magazine

--- components/shared/our-magazine.jsx
+++ components/shared/our-magazine.jsx
@@ -6,7 +6,7 @@ import {getLatestNewsPdf} from "@/lib/fetchData";
 import {format} from "date-fns";
 import {cn} from "@/lib/utils"
 
-const OurMagazine = async ({className}) => {
+const OurMagazine = async ({className, cardClass}) => {
 
     const latestPdfNews = await getLatestNewsPdf();
 
@@ -15,11 +15,13 @@ const OurMagazine = async ({className}) => {
     return (
        <>
            {latestPdfNews?.pdf_file &&
-               <div className={cn("flex flex-col gap-4 justify-center bg-white border border-[#E0EBFF] py-4 px-5 rounded-lg", className)}>
-                   <h4 className="text-center font-bold">Наша газета</h4>
-                   <NewsMagazineCard pdfUrl={latestPdfNews} className="w-[227px] h-[324px]" type="main"/>
-                   <Link href={`/archive-releases/${currentYear}`}>
-                       <Button variant="ghost" className="text-[#101828] font-medium text-sm border border-[#E0EBFF] rounded-lg py-[18px] w-full h-full">Архив выпусков</Button>
+               <div className={cn("lg:bg-white bg-transparent lg:border border-none border-[#E0EBFF] py-4 px-5 rounded-lg", className)}>
+                   <div className="flex flex-col gap-4 justify-center sm:items-center mb-4">
+                       <h4 className="md:text-center lg:text-base text-[22px] text-left font-bold">Наша газета</h4>
+                       <NewsMagazineCard pdfUrl={latestPdfNews} className={cn("w-[227px] h-[324px]", cardClass)} type="main"/>
+                   </div>
+                   <Link href={`/archive-releases/${currentYear}`} className="flex items-center justify-center">
+                       <Button variant="ghost" className="text-[#101828] max-w-[388px] font-medium text-sm border border-[#E0EBFF] rounded-lg py-[18px] w-full h-full">Архив выпусков</Button>
                    </Link>
                </div>
            }
